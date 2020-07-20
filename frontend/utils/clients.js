0777a1eaa36d8bb09095f674aaeac722f5542af0
unknown <honey.narendran@balzeclan.com>
2020-07-20 16:26:45 +0530

    bug fixes

--- frontend/utils/clients.js
+++ frontend/utils/clients.js
@@ -1,7 +1,16 @@
 const request = require('request')
 
+// let ec2 = require("ec2-publicip");
+// const IP;
+// ec2.getPublicIP( (error, ip) => {
+//   if (error) console.log(error);
+//   IP = ip
+//   console.log("Instance Public IP: ", ip);
+// });
+
+
 const clients = (callback) => {
-    const url = 'http://0.0.0.0:8087/clients'
+    const url = `http://54.211.89.96:8087/clients`
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
